8c29ce13cfefc9c90379dc78d7728ad381a82dcf
Stef Claus <stef.claus@gmail.com>
2019-01-06 20:51:37 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,10 +36,7 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category, SUM(pledges.amount)
   FROM projects JOIN pledges ON pledges.project_id = projects.id
-  WHERE projects.category = "music"
-  GROUP BY projects.title
-  HAVING SUM(pledges.amount) >= projects.funding_goal
-  ORDER BY SUM(pledges.amount);"
+  WHERE projects.category = "music";"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
